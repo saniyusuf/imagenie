8d06aed808174936c92bc4341a6de9ce47a219ce
Sani Yusuf <saniyusuf@gmail.com>
2015-07-23 00:57:20 +0100

    hack

--- karma.conf.js
+++ karma.conf.js
@@ -65,6 +65,11 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
+    singleRun: false,
+
+      plugins:[
+          'karma-jasmine',
+          'karma-phantomjs-launcher'
+      ]
   });
 };
